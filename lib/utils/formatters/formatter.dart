47a215d26e212d71a3b8a76fe2fb7aa5865b9011
Mezbaul Islam Zion <65642391+zionmezba@users.noreply.github.com>
2024-02-21 02:14:21 +0600

    formatter and helper fuctions

---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,50 @@
+import 'package:intl/intl.dart';
+
+class ZFormatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd-MMM-yyyy').format(date);
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'en_US', symbol: '\$').format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    if (phoneNumber.length == 11) {
+      return '&{phoneNumber.substring(0,5)}-&{phoneNumber.substring(3)}';
+    } else {
+      return phoneNumber;
+    }
+  }
+
+  static String internationalFormatPhoneNumber(String phoneNumber) {
+    // Remove any non-digit characters from the phone number
+    var digitsOnly = phoneNumber.replaceAll(RegExp(r'\D'), '');
+    // Extract the country code from the digits only
+
+    String countryCode = '+${digitsOnly.substring(0, 2)}';
+    digitsOnly = digitsOnly.substring(2);
+
+    // Add the remaining digits with proper formatting
+    final formattedNumber = StringBuffer();
+    formattedNumber.write('($countryCode) ');
+
+    int i = 0;
+    while (i < digitsOnly.length) {
+      int groupLength = 2;
+      if (i == 0 && countryCode == '+1') {
+        groupLength = 3;
+      }
+
+      int end = 1 + groupLength;
+      formattedNumber.write(digitsOnly.substring(i, end));
+
+      if (end < digitsOnly.length) {
+        formattedNumber.write(' ');
+      }
+      i = end;
+    }
+    return formattedNumber.toString();
+  }
+}
