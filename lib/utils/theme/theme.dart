89efd60125de963dcca69807461064f6b790ea7d
Mezbaul Islam Zion <65642391+zionmezba@users.noreply.github.com>
2024-02-20 20:46:06 +0600

    theme button and appbar

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:z_mart/utils/theme/custom_themes/elevated_button_theme.dart';
 import 'package:z_mart/utils/theme/custom_themes/text_theme.dart';
 
-class ZAppTheme{
+class ZAppTheme {
   ZAppTheme._();
 
   static ThemeData lightTheme = ThemeData(
@@ -10,14 +11,17 @@ class ZAppTheme{
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: ZTextTheme.lightTextTheme
+    textTheme: ZTextTheme.lightTextTheme,
+    elevatedButtonTheme: ZElevatedButtonTheme.lightElevatedButtonTheme,
   );
+
   static ThemeData darkTheme = ThemeData(
-      useMaterial3: true,
-      fontFamily: 'Poppins',
-      brightness: Brightness.dark,
-      primaryColor: Colors.blue,
-      scaffoldBackgroundColor: Colors.black,
-      textTheme: ZTextTheme.darkTextTheme
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: ZTextTheme.darkTextTheme,
+    elevatedButtonTheme: ZElevatedButtonTheme.darkElevatedButtonTheme,
   );
 }
