adcf13ed466cff7435b37a184e4cd7ec437f2628
zionmezba <mezbaul15-3496@diu.edu.bd>
2024-03-05 21:40:26 +0600

    bug fix

--- lib/features/shop/screens/brand/brand_products.dart
+++ lib/features/shop/screens/brand/brand_products.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:z_mart/common/widgets/appbar/appbar.dart';
 import 'package:z_mart/utils/constants/sizes.dart%20';
 
+import '../../../../common/widgets/brands/brand_card.dart';
 import '../../../../common/widgets/products/sortable/sortable_products.dart';
 
-class AllProductsScreen extends StatelessWidget {
-  const AllProductsScreen({super.key});
+class BrandProducts extends StatelessWidget {
+  const BrandProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: ZAppBar(title: Text('All Products'), showBackArrow: true),
+      appBar: ZAppBar(
+        title: Text('Nike'),
+        showBackArrow: true,
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(ZSizes.defaultSpace),
-          child: ZSortableProducts(),
+          child: Column(
+            children: [
+              ZBrandCard(showBorder: true),
+              SizedBox(height: ZSizes.spaceBtwSections),
+              ZSortableProducts(),
+            ],
+          ),
         ),
       ),
     );
