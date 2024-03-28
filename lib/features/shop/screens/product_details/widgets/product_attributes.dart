8618231b8343d900aee1a53ba525a9cb122f1fc3
zionmezba <mezbaul15-3496@diu.edu.bd>
2024-03-29 03:30:53 +0600

    product details page 4 done

--- lib/features/shop/screens/product_details/widgets/product_attributes.dart
+++ lib/features/shop/screens/product_details/widgets/product_attributes.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:z_mart/common/styles/rounded_container.dart';
 import 'package:z_mart/common/widgets/products/product_price.dart';
 import 'package:z_mart/common/widgets/texts/section_heading.dart';
+import 'package:z_mart/features/shop/controllers/product/variation_controller.dart';
 import 'package:z_mart/features/shop/models/product_model.dart';
 import 'package:z_mart/features/shop/screens/product_details/widgets/product_title_text.dart';
 import 'package:z_mart/utils/constants/colors.dart';
@@ -19,157 +22,131 @@ class ZProductAttributes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = ZHelperFunctions.isDarkMode(context);
-    return Column(
-      children: [
-        /// Selected Attribute pricing
-        ZRoundedContainer(
-          padding: const EdgeInsets.all(ZSizes.md),
-          backgroundColor: dark ? ZColors.darkerGrey : ZColors.grey,
-          child: Column(
-            children: [
-              ///Title Price and stock stat
-              Row(
+    final controller = Get.put(VariationController());
+
+    return Obx(
+      () => Column(
+        children: [
+          /// Selected Attribute pricing
+          /// display variation price and stock when some variation is selected
+          if (controller.selectedVariation.value.id.isNotEmpty)
+            ZRoundedContainer(
+              padding: const EdgeInsets.all(ZSizes.md),
+              backgroundColor: dark ? ZColors.darkerGrey : ZColors.grey,
+              child: Column(
                 children: [
-                  const ZSectionHeading(
-                    title: 'Variation',
-                    showActionButton: false,
-                  ),
-                  const SizedBox(
-                    width: ZSizes.spaceBtwItems,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  ///Title Price and stock stat
+                  Row(
                     children: [
-                      Row(
-                        children: [
-                          const ZProductTitleText(
-                            title: 'Price : ',
-                            smallSize: true,
-                          ),
-
-                          ///Actual Price
-                          Text(
-                            '${ZTexts.currencyBDT}1200',
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .apply(decoration: TextDecoration.lineThrough),
-                          ),
-                          const SizedBox(
-                            width: ZSizes.spaceBtwItems,
-                          ),
-
-                          ///Sale Price
-                          const ZProductPriceText(price: '980'),
-                        ],
+                      const ZSectionHeading(
+                        title: 'Variation',
+                        showActionButton: false,
                       ),
-
-                      ///Stock
-                      Row(
+                      const SizedBox(width: ZSizes.spaceBtwItems),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const ZProductTitleText(
-                            title: 'Stock : ',
-                            smallSize: true,
+                          Row(
+                            children: [
+                              const ZProductTitleText(
+                                title: 'Price : ',
+                                smallSize: true,
+                              ),
+
+                              ///Actual Price
+                              if (controller.selectedVariation.value.salePrice >
+                                  0)
+                                Text(
+                                  '${ZTexts.currencyBDT}${controller.selectedVariation.value.price}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .apply(
+                                          decoration:
+                                              TextDecoration.lineThrough),
+                                ),
+                              const SizedBox(width: ZSizes.spaceBtwItems),
+
+                              ///Sale Price
+                              ZProductPriceText(
+                                  price: controller.getVariationPrice())
+                            ],
                           ),
-                          Text(
-                            'In Stock',
-                            style: Theme.of(context).textTheme.titleMedium,
+
+                          ///Stock
+                          Row(
+                            children: [
+                              const ZProductTitleText(
+                                title: 'Stock : ',
+                                smallSize: true,
+                              ),
+                              Text(
+                                controller.variationStockStatus.value,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              )
+                            ],
                           )
                         ],
-                      )
+                      ),
                     ],
                   ),
+                  ZProductTitleText(
+                    title: controller.selectedVariation.value.description ?? '',
+                    smallSize: true,
+                    maxLines: 4,
+                  )
                 ],
               ),
-              const ZProductTitleText(
-                title:
-                    'This is the description of the product and this can go up to 4 lines',
-                smallSize: true,
-                maxLines: 4,
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: ZSizes.spaceBtwItems,
-        ),
+            ),
+          const SizedBox(height: ZSizes.spaceBtwItems),
 
-        ///--Attributes
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const ZSectionHeading(
-              title: 'Colors',
-              showActionButton: false,
-            ),
-            const SizedBox(
-              height: ZSizes.spaceBtwItems / 2,
-            ),
-            Wrap(
-              spacing: 8,
-              children: [
-                ZChoiceChip(
-                  text: 'Red',
-                  selected: true,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'Green',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'Blue',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-              ],
-            )
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const ZSectionHeading(
-              title: 'Sizes',
-              showActionButton: false,
-            ),
-            const SizedBox(
-              height: ZSizes.spaceBtwItems / 2,
-            ),
-            Wrap(
-              spacing: 8,
-              children: [
-                ZChoiceChip(
-                  text: 'EU 34',
-                  selected: true,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'EU 36',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'EU 38',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'EU 40',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-                ZChoiceChip(
-                  text: 'EU 42',
-                  selected: false,
-                  onSelected: (value) {},
-                ),
-              ],
-            )
-          ],
-        ),
-      ],
+          ///--Attributes
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: product.productAttributes!
+                .map((attribute) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ZSectionHeading(
+                          title: attribute.name ?? '',
+                          showActionButton: false,
+                        ),
+                        const SizedBox(height: ZSizes.spaceBtwItems / 2),
+                        Obx(
+                          () => Wrap(
+                              spacing: 8,
+                              children: attribute.values!.map((attributeValue) {
+                                final isSelected = controller
+                                        .selectedAttributes[attribute.name] ==
+                                    attributeValue;
+                                final available = controller
+                                    .getAttributesAvailabilityInVariation(
+                                        product.productVariations!,
+                                        attribute.name!)
+                                    .contains(attributeValue);
+
+                                return ZChoiceChip(
+                                  text: attributeValue,
+                                  selected: isSelected,
+                                  onSelected: available
+                                      ? (selected) {
+                                          if (selected && available) {
+                                            controller.onAttributeSelected(
+                                                product,
+                                                attribute.name ?? '',
+                                                attributeValue);
+                                          }
+                                        }
+                                      : null,
+                                );
+                              }).toList()),
+                        )
+                      ],
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
     );
   }
 }
